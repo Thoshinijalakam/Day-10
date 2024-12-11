2ee102295d36908129dd6e9b408de3d94c79bcd9
Thoshinijalakam <thoshinijalakam@gmail.com>
2024-12-11 13:42:02 +0530

    Add files via upload

---
+++ Staticprotected3.java
@@ -0,0 +1,10 @@
+public class Staticprotected3 {
+    protected static void s(){
+        System.out.println("Hello students");
+    }
+    public static void main(String args[]){
+    s();
+    }
+    
+}
+/*  Hello students */
