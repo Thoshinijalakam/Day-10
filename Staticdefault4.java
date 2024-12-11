84d5822cb1a15675da19408ba2cc72b8e2c8d7af
Thoshinijalakam <thoshinijalakam@gmail.com>
2024-12-11 13:40:52 +0530

    Add files via upload

---
+++ Staticdefault4.java
@@ -0,0 +1,11 @@
+public class Staticdefault4 {
+    static String s(){
+        return"Hello students";    
+    }
+    public static void main(String args[]){
+        String res=s();
+        System.out.println(res);
+
+    }
+}
+/* Hello students */
