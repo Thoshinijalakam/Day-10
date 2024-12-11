55668be28e0cb8d94fb04190af81a0c598302434
Thoshinijalakam <thoshinijalakam@gmail.com>
2024-12-11 13:41:09 +0530

    Add files via upload

---
+++ Staticnovoid.java
@@ -0,0 +1,12 @@
+public class Staticnovoid {
+    public static String s()
+    {
+        return "Hello students";
+    }
+    public static void main(String args[]){
+        String res=s();
+        System.out.println(res);
+    }
+    
+}
+/*  Hello students */
