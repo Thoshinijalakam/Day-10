13e6337ee346988ab42a7e31850ae74ba6a91799
Thoshinijalakam <thoshinijalakam@gmail.com>
2024-12-11 13:41:43 +0530

    Add files via upload

---
+++ Staticprivate2.java
@@ -0,0 +1,10 @@
+public class Staticprivate2 {
+    private static void s()
+    {
+        System.out.println("Hello students");
+    }
+    public static void main(String args[]){
+        s();
+    }
+}    
+/* Hello students */
